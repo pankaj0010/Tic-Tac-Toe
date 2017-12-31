8fd01a5545d36472647ce7a33edac882b044c2ed
Pankaj Kumar <pankaj0010g@gmail.com>
2017-12-31 20:03:55 +0530

    added New Game feature

--- GameInterface.java
+++ GameInterface.java
@@ -9,10 +9,10 @@ import java.awt.Color;
 import java.awt.Font;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-public class GameInterface
+public class GameInterface implements ActionListener
 {
-    Board board;
-    IOHandler IOhandler;
+    private Board board;
+    private static IOHandler IOhandler;
     private static JButton button[][]=new JButton[3][3];
     JButton textButton=new JButton();
     public GameInterface() 
@@ -20,25 +20,49 @@ public class GameInterface
         board = new BoardImpl();
         IOhandler = new IOHandler(board,textButton);
     }
+    public void actionPerformed(ActionEvent e)
+    {
+        startNewGame();
+    }
+    private void startNewGame()
+    {
+        deactivateGame();
+        board=new BoardImpl();
+        textButton.setText("X's turn");
+        IOhandler = new IOHandler(board,textButton);
+        for(int i=0;i<3;i++){
+            for(int j=0;j<3;j++){
+                button[i][j].setEnabled(true);
+                button[i][j].setText("");
+                button[i][j].addActionListener(IOhandler);
+            }
+        }
+    }
     public static void deactivateGame()
     {
-        for(int i=0;i<3;i++)
-            for(int j=0;j<3;j++)
+        for(int i=0;i<3;i++){
+            for(int j=0;j<3;j++){
                 button[i][j].setEnabled(false);
+                button[i][j].removeActionListener(IOhandler);
+            }
+        }
     }
     private void init()
     {
         JFrame frame=new JFrame("Tic Tac Toe");
         JPanel boardPanel=new JPanel();
         JPanel textPanel=new JPanel();
+        JButton newButton=new JButton("New Game");
 
         JPanel mainPanel=new JPanel();
         boardPanel.setSize(new Dimension(300,240));
         textPanel.setSize(new Dimension(300,60));
 
+        newButton.addActionListener(this);
+        
         mainPanel.setLayout(new BorderLayout());
         boardPanel.setLayout(new GridLayout(3,3));
-        textPanel.setLayout(new BorderLayout());  
+        textPanel.setLayout(new GridLayout(1,2));  
 
         
         textButton.setText("X's turn");
@@ -47,6 +71,7 @@ public class GameInterface
         textButton.setRolloverEnabled(false);
         textButton.setOpaque(true);
         textPanel.add(textButton);
+        textPanel.add(newButton);
 
         mainPanel.add(boardPanel, BorderLayout.CENTER);
         mainPanel.add(textPanel, BorderLayout.SOUTH);
@@ -54,8 +79,7 @@ public class GameInterface
               
         frame.setSize(300,300);
         frame.add(mainPanel);
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        IOHandler actions = new IOHandler(board,textButton); 
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);     
         for(int i=0;i<3;i++){
             for(int j=0;j<3;j++){
                 button[i][j]=new JButton();
